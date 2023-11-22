47f2d355ba48011470fd31bf2f0851909a9a5d22
Dmitrii <rakhmanin.dm@gmail.com>
2023-11-22 15:45:42 +0300

    ref: add translation to header button and intro

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
-import Line from '../Line/Line';
-import Navigation from '../Navigation/Navigation';
-import logo from '../../images/logo.svg';
-import './Header.scss';
-import HeaderLine1 from '../../images/header-line1.svg?react';
-import  HeaderLine2 from '../../images/header-line2.svg?react';
+import React, { useState } from "react";
+import Line from "../Line/Line";
+import Navigation from "../Navigation/Navigation";
+import logo from "../../images/logo.svg";
+import "./Header.scss";
+import HeaderLine1 from "../../images/header-line1.svg?react";
+import HeaderLine2 from "../../images/header-line2.svg?react";
+import { Trans, useTranslation } from "react-i18next";
 
 const Header: React.FC = () => {
   const [isMenuOpened, setIsMenuOpened] = useState(false);
+  const { t } = useTranslation();
 
-  const classButtonMenu = `header__button-menu${isMenuOpened ? ' header__button-menu_opened' : ''}`;
-  const classNav = `header__nav${isMenuOpened ? ' header__nav_opened' : ''}`;
+  const classButtonMenu = `header__button-menu${
+    isMenuOpened ? " header__button-menu_opened" : ""
+  }`;
+  const classNav = `header__nav${isMenuOpened ? " header__nav_opened" : ""}`;
   const classLink = `link header__nav-link`;
 
   const handleOnButtonMenu = () => {
@@ -27,19 +31,29 @@ const Header: React.FC = () => {
       <a href="/" className="header__logo-link">
         <img src={logo} className="header__logo" alt="Логотип" />
       </a>
-      <button type="button" className={classButtonMenu} onClick={handleOnButtonMenu}>
+      <button
+        type="button"
+        className={classButtonMenu}
+        onClick={handleOnButtonMenu}
+      >
         <span className="header__button-menu-icon" />
       </button>
-      <Navigation classNameNav={classNav} classNameLink={classLink} onClick={handleOnNavLink} />
+      <Navigation
+        classNameNav={classNav}
+        classNameLink={classLink}
+        onClick={handleOnNavLink}
+      />
       <a href="#feedback" className="button header__button-link">
-        Обсудить проект
+        {t("header.button")}
       </a>
       <h1 className="header__title">
-        <span className="header__title-span">
-          Разрабатываем и&nbsp;внедряем веб&#8209;приложения
-        </span>
-        , которые помогают оптимизировать бизнес-процессы и&nbsp;решить сложные управленческие
-        задачи
+        <Trans i18nKey="header.intro">
+          <span className="header__title-span">
+            Разрабатываем и внедряем веб приложения
+          </span>
+          , которые помогают оптимизировать бизнес-процессы и решить сложные
+          управленческие задачи
+        </Trans>
       </h1>
       <Line lineSVG={HeaderLine2} classLineSVG="header__line2" />
     </header>
