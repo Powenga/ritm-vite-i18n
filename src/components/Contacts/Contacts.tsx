0c27dc46f232e2e372619dff3cbb73f9867ff54e
Dmitrii <rakhmanin.dm@gmail.com>
2023-11-22 22:38:19 +0300

    ref: add translation to contact section

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import  ContactLine from '../../images/contact-line.svg?react';
-import Line from '../Line/Line';
-import './Contacts.scss';
+import React from "react";
+import ContactLine from "../../images/contact-line.svg?react";
+import Line from "../Line/Line";
+import "./Contacts.scss";
+import { useTranslation } from "react-i18next";
 
 const Contacts: React.FC = () => {
+  const { t } = useTranslation();
   return (
     <section id="contacts" className="section contacts">
       <div className="section__wrapper contacts__wrapper">
         <Line lineSVG={ContactLine} classLineSVG="contacts__line" />
-        <h2 className="section__title">Контакты</h2>
-        <h3 className="section__subtitle contacts__subtitle">Как с нами связаться</h3>
+        <h2 className="section__title">{t("contacts.title")}</h2>
+        <h3 className="section__subtitle contacts__subtitle">
+          {t("contacts.subtitle")}
+        </h3>
         <div className="contacts__list">
-          <p className="contacts__item">Контактное лицо: Вадим&nbsp;Бакуров, CEO</p>
-          <a href="tel: +79623500014" title="Позвонить" className="contacts__item">
+          <p className="contacts__item">{t("contacts.person.name")}</p>
+          <a
+            href={`tel: ${t("contacts.person.phone.number")}`}
+            title="Позвонить"
+            className="contacts__item"
+          >
             <span className="contacts__item-icon contacts__item-icon_type_mobile" />
-            +7 (962) 350-00-14
+            {t("contacts.person.phone.placeholder")}
           </a>
-          <a href="mailto: vb@ritm.studio" title="Отправить почту" className="contacts__item">
+          <a
+            href={`mailto: ${t("contacts.person.email")}`}
+            title="Отправить почту"
+            className="contacts__item"
+          >
             <span className="contacts__item-icon contacts__item-icon_type_email" />
-            vb@ritm.studio
+            {t("contacts.person.email")}
           </a>
         </div>
       </div>
