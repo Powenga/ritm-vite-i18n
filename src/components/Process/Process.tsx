bd057b6eb3bcf3e97695f9affaa93bfe5c850c07
Dmitrii <rakhmanin.dm@gmail.com>
2023-11-22 22:00:22 +0300

    ref: add translation to process section

--- src/components/Process/Process.tsx
+++ src/components/Process/Process.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import '../../styles/section.scss';
-import './Process.scss';
-import Steps from './Steps/Steps';
+import React from "react";
+import "../../styles/section.scss";
+import "./Process.scss";
+import Steps from "./Steps/Steps";
+import { useTranslation } from "react-i18next";
 
 const Process: React.FC = () => {
+  const { t } = useTranslation();
   return (
     <section id="process" className="section process">
       <div className="section__wrapper">
-        <h2 className="section__title process__title">Процесс работы</h2>
+        <h2 className="section__title process__title">{t("process.title")}</h2>
         <Steps />
       </div>
     </section>
--- src/components/Process/Steps/Steps.tsx
+++ src/components/Process/Steps/Steps.tsx
@@ -1,27 +1,32 @@
-import React from 'react';
-import './Steps.scss';
-import STEPS from './constants';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import "./Steps.scss";
 
 const Steps: React.FC = () => {
-  const stepItems = STEPS.map((item, index) => {
-    const stepKey = `step${index}`;
-    return (
-      <li key={stepKey} className="section__item steps__item">
-        <div className="steps__item-number-wrapper">
-          <p className="steps__item-number">{index + 1}</p>
-        </div>
-        <p className="steps__item-title">{item.title}</p>
-        <p className="steps__item-description">{item.description}</p>
-      </li>
-    );
-  });
+  const { t } = useTranslation();
+  const stepItems = t("process.steps.list", { returnObjects: true }).map(
+    (item, index) => {
+      const stepKey = `step${index}`;
+      return (
+        <li key={stepKey} className="section__item steps__item">
+          <div className="steps__item-number-wrapper">
+            <p className="steps__item-number">{index + 1}</p>
+          </div>
+          <p className="steps__item-title">{item.title}</p>
+          <p className="steps__item-description">{item.description}</p>
+        </li>
+      );
+    }
+  );
 
-  const stepListElement = <ul className="section__list steps__list">{stepItems}</ul>;
+  const stepListElement = (
+    <ul className="section__list steps__list">{stepItems}</ul>
+  );
 
   return (
     <div className="steps">
       <h3 className="section__subtitle steps__title">
-        Взаимодействие с&nbsp;нами&nbsp;состоит из&nbsp;следующих шагов
+        {t("process.steps.title")}
       </h3>
       {stepListElement}
     </div>
